a2a45fb454f36ad47018e5eedd918f08a8a85b38
il-luzhin <dmmcdougall@gmail.com>
2021-05-17 16:57:11 -0600

    docementation for db_backup

--- db_backup.py
+++ db_backup.py
@@ -3,6 +3,7 @@ import pandas as pd
 import db_functions as dbfnc
 import config as cfg
 
+# grab a list of tables form the db
 query = ("SELECT * FROM INFORMATION_SCHEMA.TABLES\
      WHERE TABLE_TYPE = 'BASE TABLE'")
 
@@ -11,11 +12,13 @@ with dbfnc.connection(cfg.my_driver, cfg.my_server, cfg.my_db) as conn:
 
 list_of_tbl = df.TABLE_NAME.to_list()
 
+# some of the tables have restricted characters, make all table names readable
 new_list = []
 for tbl in list_of_tbl:
     new_str = '['+tbl+']'
     new_list.append(new_str)
 
+# save each table as a .csv
 for i in new_list:
     query = (f'SELECT * FROM {i}')
     with dbfnc.connection(cfg.my_driver, cfg.my_server, cfg.my_db) as conn:
