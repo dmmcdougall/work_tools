dc731ad5f39e44c021d6a8b044263ff0e0cfbdcb
il-luzhin <dmmcdougall@gmail.com>
2021-05-18 11:11:10 -0600

    db_backup index=False

--- db_backup.py
+++ db_backup.py
@@ -3,30 +3,43 @@ import pandas as pd
 import db_functions as dbfnc
 import config as cfg
 
-# grab a list of tables form the db
-query = ("SELECT * FROM INFORMATION_SCHEMA.TABLES\
-     WHERE TABLE_TYPE = 'BASE TABLE'")
+def main():
+    # grab a list of tables form the db
+    query = ("SELECT * FROM INFORMATION_SCHEMA.TABLES\
+        WHERE TABLE_TYPE = 'BASE TABLE'")
 
-with dbfnc.connection(cfg.my_driver, cfg.my_server, cfg.my_db) as conn:
-    df = pd.read_sql(query, conn)
-
-list_of_tbl = df.TABLE_NAME.to_list()
-
-# some of the tables have restricted characters, make all table names readable
-new_list = []
-for tbl in list_of_tbl:
-    new_str = '['+tbl+']'
-    new_list.append(new_str)
-
-# save each table as a .csv
-for i in new_list:
-    query = (f'SELECT * FROM {i}')
     with dbfnc.connection(cfg.my_driver, cfg.my_server, cfg.my_db) as conn:
         df = pd.read_sql(query, conn)
-    
-    print(i)
-    print(df.head(5))
-    print()
-    
-    df.to_csv(f'{cfg.desktop_dir}/{i}.csv')
 
+    list_of_tbl = df.TABLE_NAME.to_list()
+
+    # some of the tables have restricted characters, make all table names readable
+    new_list = []
+    for tbl in list_of_tbl:
+        new_str = '['+tbl+']'
+        new_list.append(new_str)
+
+    # save each table as a .csv
+    for i in new_list:
+        query = (f'SELECT * FROM {i}')
+        with dbfnc.connection(cfg.my_driver, cfg.my_server, cfg.my_db) as conn:
+            df = pd.read_sql(query, conn)
+        
+        print(i)
+        print(df.head(5))
+        print()
+        
+        df.to_csv(f'{cfg.desktop_dir}/{i}.csv', index=False)
+
+if __name__ == '__main__':
+    print()
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print("                      file launched")
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print()
+    main()
+    print()
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print("                        VICTORY!")
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print()
